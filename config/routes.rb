70ff0fe9f30fba4325572780d415c3632385f0fe
nfsantibanez <nfsantibanez@uc.cl>
2018-10-05 11:23:54 -0300

    retoques pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'main_pages/main_1'
-  get 'main_pages/main_2'
-  get 'main_pages/main_3'
+  # get 'main_pages/main_1'
+  # Home HomePage
+  root to: 'main_pages#main_1'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
